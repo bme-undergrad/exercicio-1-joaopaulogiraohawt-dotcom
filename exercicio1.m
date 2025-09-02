e7b54849dcf726a9bdde58992e8ff0e30d2224be
joaopaulogiraohawt-dotcom <joaopaulogiraohawt@gmail.com>
2025-09-02 12:41:48 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -7,31 +7,22 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    t_raizes = zeros(imax, 1);
-    erro = zeros(imax, 1);
+t = zeros(imax,1)
+t(1) = x0
 
-    % Definicao da iteracao inicial generica.
-    t_raizes(1) = x0;
+for ii = 1:length(t)-1
+  ii
+  if ii ~= 1
+    erro(ii) = abs((t(ii)-t(ii-1))/t(ii))
+    if erro(ii) < es
+      break
+    endif
+  endif
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii))
+endfor
 
-    % Iteracoes subsequentes utilizando o Metodo de Newton-Raphson.
-    for ii = 1: length (t_raizes)-1
-        % Calculo da proxima aproximacao do valor do tempo [t(ii+1)].
-        t_raizes(ii+1) = t_raizesii) - func(t_raizes(ii)) / func_d(t_raizes(ii))
-
-        % Determinacao e armazenamneto do erro na iteracao (em porcentagem).
-        if t_raizes(ii+1) ~= 0
-            erro(ii) = abs((t_raizes(ii+1) - t_raizes(ii)) / t_raizes(ii+1)) * 100;
-        end
-
-        % Verificacao do erro para possivelmente terminar o processo antecipadamente.
-        if erro(ii) < es
-            t_raizes = t_raizes(ii+1); % Obtem o valor otimo de v.
-            break;
-        end
-    end
-
-    % Caso o loop nao atinja um erro menor que 1% durante as 20 iteracoes, usa-se o valor maximo obtido.
-    t = t_raizes(ii);
+disp('O valor é: ')
+t = t(ii)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
